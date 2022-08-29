8056a1cfee5cbce8cb3a0cc83f86ffded1d242ef
Alec Krafzik <alec.krafzik@grover.com>
2022-08-29 15:36:22 -0300

    added social

--- components/footer.js
+++ components/footer.js
@@ -6,13 +6,19 @@ function Footer() {
     <footer>
       <div className="bg-dark-gray flex flex-col justify-center items-center text-center p-2 border-t-2 border-dark-red">
         <div className="flex gap-6">
-          <h1 className="font-semibold">
-            Build with ❤ by Alec Krafzik
-          </h1>
-          <FaInstagram className="text-2xl cursor-pointer hover:text-purple-600" />
-          <FaFacebook className="text-2xl cursor-pointer hover:text-blue-600" />
-          <FaLinkedin className="text-2xl cursor-pointer hover:text-blue-600" />
-          <FaGithub className="text-2xl cursor-pointer hover:text-blue-600" />
+          <h1 className="font-semibold">Build with ❤ by Alec Krafzik</h1>
+          <a href="https://www.instagram.com/akrafzik/">
+            <FaInstagram className="text-2xl cursor-pointer hover:text-purple-600" />
+          </a>
+          <a href="https://www.facebook.com/AlecKrafzik/">
+            <FaFacebook className="text-2xl cursor-pointer hover:text-blue-600" />
+          </a>
+          <a href="https://www.linkedin.com/in/aleckfzk/">
+            <FaLinkedin className="text-2xl cursor-pointer hover:text-blue-600" />
+          </a>
+          <a href="https://github.com/akrafzik">
+            <FaGithub className="text-2xl cursor-pointer hover:text-blue-600" />
+          </a>
         </div>
       </div>
     </footer>
